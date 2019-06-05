261acccb6f52d3dba074a26e9451196d32f38e06
Kiszona <grzegorzpodlaski@gmail.com>
2019-06-05 19:52:13 +0200

    dodawanie pracownika do bazy danych(w trakcie)

--- dodajpracownika.php
+++ dodajpracownika.php
@@ -20,7 +20,7 @@ if ($polaczenie->connect_errno!=0)
 {
     echo "Error: ".$polaczenie->connect_errno;
 }
-
+//sprawdzenie imienia
 $blad = false;
 if(isset($_GET['imie'])){
     if (strlen($_GET['imie']) == 0){
@@ -28,17 +28,22 @@ if(isset($_GET['imie'])){
         echo "nie podano imienia!<br/>";
     }else array_push($dane, $_GET['imie']);
 }else $blad = true;
+//sprawdzenie nazwiska
 if(isset($_GET['nazwisko'])){
     if (strlen($_GET['nazwisko']) == 0){
         $blad = true;
         echo "nie podano nazwiska!";
     }else array_push($dane, $_GET['nazwisko']);
 }else $blad = true;
+//jesli nie popelniono bledu
 if (!$blad){
-    array_push($dane, substr($_GET['imie'],0,3).
-        substr($_GET['nazwisko'],0,3));
-    echo "login: ".$dane[2]." haslo: ".rand(1000,9999);
+    array_push($dane, substr($_GET['imie'],0,strlen($_GET['imie'])).
+        substr($_GET['nazwisko'],0,strlen($_GET['nazwisko'])));
 
+    echo "login: ".$dane[2]." haslo: ".rand(1000,9999);
+$sql = "INSERT INTO `uzytkownik` (`idUzytkownik`, `haslo`,
+ `imie`, `nazwisko`, `login`, `stanowisko`)
+  VALUES (NULL, '', '', '', '', 'Inne')";
 }
 ?>
 
@@ -55,10 +60,16 @@ if (!$blad){
 <fieldset>
     <legend align="left">DANE NOWEGO PRACOWNIKA</legend>
     <form action="dodajpracownika.php">
-        <input type="text" name="imie" > :IMIE<br/>
-        <input type="text" name="nazwisko" > :NAZWISKO<br/>
-<!--        <input type="hidden" name=rand(1000,9999)> HASLO-->
-
+        <input type="text" name="imie" > :IMIE<br/><br/>
+        <input type="text" name="nazwisko" > :NAZWISKO<br/><br/>
+        <select name="stanowisko">
+            <option value="" selected></option>
+            <option value="Manager">MANAGER</option>
+            <option value="Kelner">KELNER</option>
+            <option value="Kucharz">KUCHARZ</option>
+            <option value="Inne">INNE</option>
+        </select>
+        STANOWISKO<br/><br/>
         <input type="submit" value="POTWIERDZ">
     </form>
 
